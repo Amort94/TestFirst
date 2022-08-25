d4672abf8a57fb178ffcb7b8accd39119100072d
Amort94 <111119587+Amort94@users.noreply.github.com>
2022-08-25 13:36:44 +0430

    Add files via upload

---
+++ My Jupyter Notebook on IBM Watson Studio.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# __Abdolali Mortazavi__ <br>
+# My desired occupation is to work as a data scientist in the marketing field:)
+
+# I am interested in data science since it could bring me a good opportunity in the competitive job market of the future.
+
+# ### This simple line of could should print out "Hello Data Science World...I am coming for you!"
+
+# In[2]:
+
+
+print("Hello Data Science World...I am coming for you!")
+
+
+# |Header 1|Header 2|Header 3|
+# |---|---|---|
+# |Cell 11|Cell 12|Cell 13|
+# 
+# > This is a sample horizontal rule <br>
+# 
+# This a numbered list: <br>
+# 1. a
+# 2. b
+# 3. c
+#     1. c1
+#     2. c2
+# 
+# <a href="https://www.google.com/webhp"> A Sample Link to Google <a>
+
+# In[ ]:
+
+
+
+
